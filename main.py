bf6acdc6d55ba469d8ccc4d877ed3c69e8f051b9
Felipe Meganha <felipengmec@gmail.com>
2022-06-17 10:01:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,44 +23,9 @@ from keras.preprocessing.image import ImageDataGenerator
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
-def download_file_from_google_drive(id, destination):
-    URL = "https://drive.google.com/file/d/19Rg9Ki7-AD1rCcrbGIjgZc2__5OeaTX2/view?usp=sharing"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-
 
 def main():
     
-    file_id = '19Rg9Ki7-AD1rCcrbGIjgZc2__5OeaTX2'
-    destination = os.getcwd()
-    download_file_from_google_drive(file_id, destination)
     
     html_temp = """
     <div style="background-color:#025246 ;padding:10px">
@@ -71,7 +36,7 @@ def main():
 
     st.markdown(html_temp, unsafe_allow_html=True)
     TamCabeca = st.text_input("Qual o volume da cabeça (cm³)?")
-    st.text(destination)
+    st.text(os.getcwd())
 
 if __name__ == '__main__':
     main()

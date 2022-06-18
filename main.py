40ea36cf5a4a71a4b8292bb0b3ef720da983fb47
Felipe Meganha <felipengmec@gmail.com>
2022-06-18 12:36:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 newpath = r'/app/deploy_vgg/Model' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
+    
+#os.listdir(r'/app/deploy_vgg/')
 
 
 def main():
@@ -38,7 +40,7 @@ def main():
 
     st.markdown(html_temp, unsafe_allow_html=True)
     TamCabeca = st.text_input("Qual o volume da cabeça (cm³)?")
-    st.text(os.listdir(r'/app/deploy_vgg/'))
+    st.text(os.listdir(r'/app/deploy_vgg/')[-1])
     #os.path.abspath(r'/app/deploy_vgg/Model/myfile.h5')
     
 
